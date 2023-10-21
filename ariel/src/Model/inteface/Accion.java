85cd44165c8199abacea89ac6f01d07dae997cc2
Y-S-500 <113991827+Y-S-500@users.noreply.github.com>
2023-10-21 15:21:50 -0500

    Add files via upload
    ariel projet

---
+++ ariel/src/Model/inteface/Accion.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.inteface;
+
+/**
+ *
+ * @author bonil
+ */
+
+
+public interface Accion {
+
+    void Agregar();
+
+    void Modificar();
+
+    void Eliminar();
+
+    Object Consultar();
+
+    String ConsultarWhereAnd();
+}
+
